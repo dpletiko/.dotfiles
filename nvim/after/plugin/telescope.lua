1daa4f01ad5424f3b4b76137e9a9080ae2f467e7
Dino Pletikosa <dino.pletikosa@gmail.com>
2023-01-05 23:49:20 +0100

    added nvim config

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,32 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+
+telescope.setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-Up>"] = actions.cycle_history_prev,
+                ["<C-Down>"] = actions.cycle_history_next
+            }
+        }
+    }
+}
+
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>pl', builtin.loclist, {})
+vim.keymap.set('n', '<leader>pq', builtin.quickfix, {})
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep For > ") })
+end)
+vim.keymap.set('n', '<leader>pw', function()
+	builtin.grep_string({ search = vim.fn.expand("<cword>") })
+end)
+vim.keymap.set('n', '<leader>pg', function()
+	telescope.extensions.live_grep_args.live_grep_args()
+end)
+
---
+++ nvim/lua/user/packer.lua
@@ -0,0 +1,84 @@
+vim.cmd.packadd('packer.nvim')
+
+local packer = require("packer")
+
+-- Have packer use a popup window
+packer.init({
+		display = {
+			open_fn = function()
+				return require('packer.util').float({ border = 'single' })
+			end
+		}
+	}
+)
+
+return packer.startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = {
+			{'nvim-lua/plenary.nvim'},
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+		},
+		config = function()
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("live_grep_args")
+		end
+	}
+
+	use { "ellisonleao/gruvbox.nvim" }
+
+	use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+
+	-- Track the engine.
+	-- use("SirVer/ultisnips")
+	-- Snippets are separated from the engine. Add this if you want them:
+	-- use("honza/vim-snippets")
+
+	use("mattn/emmet-vim")
+	use("jwalton512/vim-blade")
+	use("sheerun/vim-polyglot")
+
+	use("tpope/vim-commentary")
+
+	use("folke/neodev.nvim")
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
+
+	use("folke/zen-mode.nvim")
+	use("github/copilot.vim")
+
+	use("gruvbox-community/gruvbox")
+	use("sainnhe/sonokai")
+
+end)
+
